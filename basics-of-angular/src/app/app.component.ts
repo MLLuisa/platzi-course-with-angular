2b30d4f41c845c430013361f951427aea0ff82dc
Maria Luisa Lufrano <marialuisa.lufrano@icloud.com>
2022-11-03 13:01:32 +0100

    Added string interpolation exercise

--- basics-of-angular/src/app/app.component.ts
+++ basics-of-angular/src/app/app.component.ts
@@ -6,5 +6,8 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
-  title = 'basics-of-angular';
+  name = 'Ivan';
+  age = 20;
+  image = 'https://img2.freepng.es/20190123/jtv/kisspng-computer-icons-vector-graphics-person-portable-net-myada-baaranmy-teknik-servis-hizmetleri-5c48d5c2849149.051236271548277186543.jpg';
+
 }
