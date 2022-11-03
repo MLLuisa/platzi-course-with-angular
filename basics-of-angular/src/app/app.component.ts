a038d551505fc6dd85227d021983ba678517e61a
Maria Luisa Lufrano <marialuisa.lufrano@icloud.com>
2022-11-03 14:39:03 +0100

    exercise with property binding

--- basics-of-angular/src/app/app.component.ts
+++ basics-of-angular/src/app/app.component.ts
@@ -9,5 +9,10 @@ export class AppComponent {
   name = 'Ivan';
   age = 20;
   image = 'https://img2.freepng.es/20190123/jtv/kisspng-computer-icons-vector-graphics-person-portable-net-myada-baaranmy-teknik-servis-hizmetleri-5c48d5c2849149.051236271548277186543.jpg';
-
+  btnDisabled = true;
+  person = {
+    name: 'Ivan',
+    age: 20,
+    avatar: 'https://img2.freepng.es/20190123/jtv/kisspng-computer-icons-vector-graphics-person-portable-net-myada-baaranmy-teknik-servis-hizmetleri-5c48d5c2849149.051236271548277186543.jpg'
+  }
 }
