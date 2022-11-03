1fe3e2ff791b911c97bf88efe7a2227dcdb36218
Maria Luisa Lufrano <marialuisa.lufrano@icloud.com>
2022-11-03 12:37:12 +0100

    Basics concept of typescript with a new file recap

---
+++ basics-of-angular/src/app/recap.ts
@@ -0,0 +1,14 @@
+const userName: string = 'Lufra';
+
+const sum = (a: number, b: number) => {
+  return a + b;
+}
+
+sum(2, 2);
+
+class Person {
+  constructor(age: number, lastName: string) {
+  }
+}
+
+const lufra = new Person(27, 'Rossi');
